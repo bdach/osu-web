9de5abf5848d9d83ec5775ac8e80b1de6b221699
Bartłomiej Dach <dach.bartlomiej@gmail.com>
2025-03-11 14:26:49 +0100

    Convert game score component to typescript

---
+++ resources/js/interfaces/match-score-json.ts
@@ -0,0 +1,29 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+// TODO: basically none of this should exist, and is only transitory
+// while the models are worked into a uniform shape
+
+export interface MatchScoreDetails {
+  pass: boolean;
+  team: string;
+}
+
+export interface MatchScoreStatistics {
+  count_100: number;
+  count_300: number;
+  count_50: number;
+  count_geki: number;
+  count_katu: number;
+  count_miss: number;
+}
+
+export default interface MatchScore {
+  accuracy: number;
+  match: MatchScoreDetails;
+  max_combo: number;
+  mods: [string];
+  score: number;
+  statistics: MatchScoreStatistics;
+  user_id: number;
+}
---
+++ resources/js/mp-history/score.tsx
@@ -0,0 +1,103 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the GNU Affero General Public License v3.0.
+// See the LICENCE file in the repository root for full licence text.
+
+import FlagCountry from 'components/flag-country';
+import Mod from 'components/mod';
+import { route } from 'laroute';
+import * as React from 'react';
+import { formatNumber } from 'utils/html';
+import { trans } from 'utils/lang';
+import MatchScore, { MatchScoreStatistics } from '../interfaces/match-score-json';
+import Ruleset from '../interfaces/ruleset';
+import UserJson from '../interfaces/user-json';
+import { classWithModifiers } from '../utils/css';
+
+interface Props {
+  mode: Ruleset;
+  score: MatchScore;
+  users: [UserJson];
+}
+
+export default function Score(props: Props) {
+  const firstRow = ['combo', 'accuracy', 'score'];
+  const secondRow = ['count_geki', 'count_300', 'count_katu', 'count_100', 'count_50', 'count_miss'];
+
+  const user = props.users[props.score.user_id];
+
+  return (
+    <div className={'mp-history-game__player-score mp-history-player-score'}>
+      <div
+        className={'mp-history-player-score__shapes'}
+        style={{ backgroundImage: `url(/images/layout/mp-history/shapes-team-${props.score.match.team}.svg)` }} />
+      <div className={'mp-history-player-score__main'}>
+        <div className={'mp-history-player-score__info-box mp-history-player-score__info-box--user'}>
+          <div className={'mp-history-player-score__username-box'}>
+            <a
+              className={'mp-history-player-score__username'}
+              href={route('users.show', { user: user.id })}>
+              {user.username}
+            </a>
+
+            {!props.score.match.pass
+              ? <span className={'mp-history-player-score__failed'}>{trans('matches.match.failed')}</span>
+              : null}
+          </div>
+          <a
+            href={route('rankings', { country: user.country?.code, mode: props.mode, type: 'performance' })}>
+            <FlagCountry country={user.country} modifiers={'medium'} />
+          </a>
+        </div>
+        <div className={classWithModifiers('mp-history-player-score__info-box', ['stats'])}>
+          <div className={classWithModifiers('mp-history-player-score__stat-row', ['first'])}>
+            <div className={'mp-history-player-score__mods'}>
+              {props.score.mods.map((mod) => (<Mod key={mod} mod={{ acronym: mod }} />))}
+            </div>
+            {firstRow.map((m) => {
+              let modifier = 'medium';
+              let value;
+
+              switch (m) {
+                case 'combo':
+                  value = formatNumber(props.score.max_combo);
+                  break;
+
+                case 'accuracy':
+                  value = `${formatNumber(props.score.accuracy * 100, 2)}%`;
+                  break;
+
+                case 'score':
+                  modifier = 'large';
+                  value = formatNumber(props.score.score);
+                  break;
+              }
+
+              return (
+                <div key={m} className={classWithModifiers('mp-history-player-score__stat', [m])}>
+                  <span
+                    className={classWithModifiers('mp-history-player-score__stat-label', ['small'])}>{trans(`matches.match.score.stats.${m}`)}</span>
+                  <span
+                    className={classWithModifiers('mp-history-player-score__stat-number', [modifier])}>{value}</span>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className={'mp-history-player-score__stat-row'}>
+            {secondRow.map((m) => {
+              if (props.mode !== 'mania' && (m === 'count_geki' || m === 'count_katu')) {
+                return null;
+              }
+
+              return (
+                <div key={m} className={classWithModifiers('mp-history-player-score__stat', ['small'])}>
+                  <span className={classWithModifiers('mp-history-player-score__stat-label', ['large'])}>{trans(`common.score_count.${m}`)}</span>
+                  <span className={classWithModifiers('mp-history-player-score__stat-number', ['small'])}>{formatNumber(props.score.statistics[m as keyof MatchScoreStatistics])}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
